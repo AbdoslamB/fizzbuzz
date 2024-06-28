0c5d193359b714c0dbf443958fa66dd6858427e7
Abdoslam Baabbad <baabbad@bc.edu>
2024-06-28 09:01:44 -0400

    Add files via upload

---
+++ fizzbuzzSQL.sql
@@ -0,0 +1,32 @@
+WITH cte(n) AS
+(
+    SELECT 1 -- Anchor part
+    UNION ALL
+    SELECT n + 1
+    FROM cte
+    WHERE n < 15 -- Recursive part
+)
+SELECT 
+    CASE
+        WHEN n % 3 = 0 AND n % 5 = 0 THEN n || ' (Fizz Buzz)'
+        WHEN n % 5 = 0 THEN n || ' (Buzz)'
+        WHEN n % 3 = 0 THEN n || ' (Fizz)'
+        ELSE CAST(n AS VARCHAR(4))
+    END
+FROM cte;
+
+--- For a column in the data sheet 
+WITH cte(n) AS
+(
+
+select n from abdoslam_db.childrent
+
+)
+SELECT 
+    CASE
+        WHEN n % 3 = 0 AND n % 5 = 0 THEN n || ' (Fizz Buzz)'
+        WHEN n % 5 = 0 THEN n || ' (Buzz)'
+        WHEN n % 3 = 0 THEN n || ' (Fizz)'
+        ELSE CAST(n AS VARCHAR(4))
+    END
+FROM cte;
